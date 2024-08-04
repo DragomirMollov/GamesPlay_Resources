03cef15d19309f1245f09b03b95a9748b8314e7d
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2024-08-04 22:49:11 +0100

    remove pass from state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -9,11 +9,9 @@ export const useLogin = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-            const result = await login(email, password);
+            const { password: _, ...authData } = await login(email, password);
 
-           changeAuthState(result);
-
-           return result;
+           changeAuthState(authData);
     };
 
     return loginHandler;
@@ -23,11 +21,9 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const { password: _, ...authData } = await register(email, password);
 
-        changeAuthState(result);
-
-        return result;
+        changeAuthState(authData);
     };
 
     return registerHandler;
