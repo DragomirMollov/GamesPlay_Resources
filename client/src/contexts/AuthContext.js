b80d92771f7b805f14796a9aaace92d0a4041c29
DragomirMollov <44067945+DragomirMollov@users.noreply.github.com>
2024-08-04 21:39:02 +0100

    Add global auth and context

---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,9 @@
+/* eslint-disable style/indent */
+import { createContext } from 'react';
+
+export const AuthContext = createContext({
+    email: '',
+    accessToken: '',
+    isAuthenticated: false,
+    changeAuthState: (authState = {}) => null,
+});
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,13 +1,17 @@
 /* eslint-disable antfu/top-level-function */
-/* eslint-disable style/brace-style */
 /* eslint-disable style/indent */
+import { useContext } from 'react';
+
 import { login } from '../api/auth-api';
+import { AuthContext } from '../contexts/AuthContext';
 
 export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
     const loginHandler = async (email, password) => {
             const result = await login(email, password);
 
-            console.log(result);
+           changeAuthState(result);
     };
 
     return loginHandler;
